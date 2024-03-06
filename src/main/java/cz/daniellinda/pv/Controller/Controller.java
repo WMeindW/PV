3e01df38ba44bf58550f92cf566febda6849d413
Meind <105383135+WMeindW@users.noreply.github.com>
2024-03-06 15:24:50 +0100

    update

--- src/main/java/cz/daniellinda/pv/Controller/Controller.java
+++ src/main/java/cz/daniellinda/pv/Controller/Controller.java
@@ -1,13 +1,45 @@
 package cz.daniellinda.pv.Controller;
 
+import cz.daniellinda.pv.Database.Customers.Customers;
+import cz.daniellinda.pv.Database.Customers.CustomersRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 
 @org.springframework.stereotype.Controller
 public class Controller {
-    @GetMapping(value = "/", produces = "text/html")
-    public ResponseEntity<String> index() {
-        return new ResponseEntity<>("<a href=\"/order\">Objednat</a>", HttpStatus.OK);
+    @Autowired
+    private CustomersRepo customersRepo;
+
+    @GetMapping("/")
+    public ResponseEntity<String> index() throws IOException {
+        return new ResponseEntity<>(Files.readString(Path.of("src/main/resources/static/login.html"), StandardCharsets.UTF_8), HttpStatus.OK);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestParam String name, @RequestParam String surname) throws IOException {
+        if (checkLogin(name, surname)) {
+            return new ResponseEntity<>(Files.readString(Path.of("src/main/resources/static/index.html"), StandardCharsets.UTF_8) + "<script>localStorage.clear();\n" + "localStorage.setItem(\"name\", \"" + name + "\");\n" + "localStorage.setItem(\"surname\",\"" + surname + "\");\n" + "</script>", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Bad login", HttpStatus.OK);
+        }
+
+    }
+
+    private boolean checkLogin(String name, String surname) {
+        List<Customers> list = customersRepo.findAll();
+        for (Customers customer : list) {
+            if (customer.getName().equals(name + " " + surname)) return true;
+        }
+        return false;
     }
 }
--- src/main/java/cz/daniellinda/pv/Database/Customers/Customers.java
+++ src/main/java/cz/daniellinda/pv/Database/Customers/Customers.java
@@ -25,6 +25,14 @@ public class Customers {
     @OneToMany(mappedBy = "customers")
     private List<Orders> orders;
 
+    public List<Orders> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(List<Orders> orders) {
+        this.orders = orders;
+    }
+
     public CustomerTypes getCustomerTypes() {
         return customerTypes;
     }
